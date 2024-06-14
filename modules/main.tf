bfa7130412a60484a388bf9e6bfdb04be8deb3de
sailalith <lalithvoop@hotmail.com>
2024-06-14 23:15:31 +0530

    updated group_id

--- modules/main.tf
+++ modules/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm" {
   ami                     = data.aws_ami.example.id
   instance_type           = var.instance_type
-  vpc_security_group_ids  = data.aws_security_group.selected.id
+  vpc_security_group_ids  = [data.aws_security_group.selected.id]
 
   tags = {
     Name = var.tool_name
