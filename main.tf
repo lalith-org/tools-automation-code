a8d6ffc4c25f990fc4b6529af20795bc3776e66a
sailalith <lalithvoop@hotmail.com>
2024-06-15 18:12:47 +0530

    added iam role, policy and profile

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ module "tools_module" {
   for_each    = var.tools
   source      = "./modules"
 
-  instance_type = each.value["instance_type"]
-  tool_name     = each.key
-  zone_id       = var.zone_id
+  instance_type         = each.value["instance_type"]
+  tool_name             = each.key
+  zone_id               = var.zone_id
+  policy_resource_list  = each.value["policy_resource_list"]
 }
 
--- modules/main.tf
+++ modules/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "vm" {
   ami                     = data.aws_ami.example.id
   instance_type           = var.instance_type
   vpc_security_group_ids  = [data.aws_security_group.selected.id]
+  iam_instance_profile    = aws_iam_instance_profile.profile.name
 
   tags = {
     Name = var.tool_name
@@ -13,5 +14,43 @@ resource "aws_route53_record" "domain" {
   name    = var.tool_name
   type    = "A"
   ttl     = 30
-  records = [aws_instance.vm.private_ip]
+  records = [aws_instance.vm.public_ip]
+}
+
+resource "aws_iam_role" "role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name        = "${var.tool_name}-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = var.policy_resource_list
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.tool_name}_profile"
+  role = aws_iam_role.role.name
 }
