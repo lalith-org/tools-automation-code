b65497434d77494ecf3226e0b0e1a23b1ee424aa
sailalith <lalithvoop@hotmail.com>
2024-06-16 00:06:55 +0530

    added grafana

--- vars.tf
+++ vars.tf
@@ -4,6 +4,12 @@ variable "tools" {
       instance_type = "t3.micro"
       policy_resource_list = ["ec2:DescribeInstances"]
     }
+
+    grafana = {
+      instance_type = "t3.micro"
+      policy_resource_list = ["ec2:DescribeInstances"]
+    }
+
   }
 }
 
