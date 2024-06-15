702dc0d75f9b5ea312fdf3eebcd78c402afebf17
sailalith <lalithvoop@hotmail.com>
2024-06-15 19:16:12 +0530

    updated role name

--- modules/main.tf
+++ modules/main.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "domain-internal" {
 }
 
 resource "aws_iam_role" "role" {
-  name = "test_role"
+  name = "${var.tool_name}-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
