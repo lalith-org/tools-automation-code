7b836eac072c329221df8298ad2d1b3568ff70f2
sailalith <lalithvoop@hotmail.com>
2024-06-19 23:38:41 +0530

    added vault pipeline and changed dummy variable name

--- modules/vars.tf
+++ modules/vars.tf
@@ -2,6 +2,6 @@ variable "instance_type" {}
 variable "tool_name" {}
 variable "zone_id" {}
 variable "policy_resource_list"{}
-variable "dummy_policy" {
+variable "dummy_policy_list" {
   default = ["ec2:DescribeInstanceTypes"]
 }
