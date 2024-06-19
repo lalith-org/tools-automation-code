411af872639ad2ce8f81be6947f5f444017e8087
sailalith <lalithvoop@hotmail.com>
2024-06-19 23:26:46 +0530

    added terraform code and ansible role for vault

--- vars.tf
+++ vars.tf
@@ -7,9 +7,13 @@ variable "tools" {
 
     grafana = {
       instance_type = "t3.micro"
-      policy_resource_list = ["ec2:DescribeInstances"]
+      dummy_policy_list = ["ec2:DescribeInstanceTypes"]
     }
 
+    vault = {
+      instance_type = "t3.micro"
+      dummy_policy_list = ["ec2:DescribeInstanceTypes"]
+    }
   }
 }
 
