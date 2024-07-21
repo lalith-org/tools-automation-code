c3dbb5be5c432b173f8ac0bea38b59daadd7dec3
sailalith <lalithvoop@hotmail.com>
2024-07-21 22:41:03 +0530

    commented elk, grafana and prometheus for time being

--- vars.tf
+++ vars.tf
@@ -1,24 +1,24 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type = "t3.micro"
-      policy_resource_list = ["ec2:DescribeInstances"]
-    }
+#    prometheus = {
+#      instance_type = "t3.micro"
+#      policy_resource_list = ["ec2:DescribeInstances"]
+#    }
 
-    grafana = {
-      instance_type = "t3.micro"
-      policy_resource_list = []
-    }
+#    grafana = {
+#      instance_type = "t3.micro"
+#      policy_resource_list = []
+#    }
 
     vault = {
       instance_type = "t3.micro"
       policy_resource_list = []
     }
 
-    elk = {
-      instance_type = "r7i.large"
-      policy_resource_list = []
-    }
+#    elk = {
+#      instance_type = "r7i.large"
+#      policy_resource_list = []
+#    }
 
     ci = {
       instance_type = "t3.small"
