cae7049180c3416867674fb0344b8f59d6aacf83
sailalith <lalithvoop@hotmail.com>
2024-07-25 21:06:13 +0530

    added sonarqube server in tools

--- vars.tf
+++ vars.tf
@@ -20,12 +20,12 @@ variable "tools" {
 #      policy_resource_list = []
 #    }
 
-    ci = {
+    ci-server = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
 
-    sonar = {
+    sonarqube = {
       instance_type = "t3.large"
       policy_resource_list = []
     }
