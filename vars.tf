d48f73186e9f99abae8cd61e9526dc3e2779c6ee
sailalith <lalithvoop@hotmail.com>
2024-07-27 21:16:51 +0530

    commented sonarqube for time being

--- vars.tf
+++ vars.tf
@@ -25,10 +25,10 @@ variable "tools" {
       policy_resource_list = []
     }
 
-    sonarqube = {
-      instance_type = "t3.large"
-      policy_resource_list = []
-    }
+#    sonarqube = {
+#      instance_type = "t3.large"
+#      policy_resource_list = []
+#    }
 
     artifactory = {
       instance_type = "t3.large"
