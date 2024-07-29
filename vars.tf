1c2c8d106d047b47cce275aa083ecd6808d1acb4
sailalith <lalithvoop@hotmail.com>
2024-07-29 19:17:44 +0530

    fixing changes

--- vars.tf
+++ vars.tf
@@ -15,20 +15,20 @@ variable "tools" {
       policy_resource_list = []
     }
 
-#    elk = {
-#      instance_type = "r7i.large"
-#      policy_resource_list = []
-#    }
+    elk = {
+      instance_type = "r7i.large"
+      policy_resource_list = []
+    }
 
     ci-server = {
       instance_type = "t3.small"
       policy_resource_list = []
     }
 
-#    sonarqube = {
-#      instance_type = "t3.large"
-#      policy_resource_list = []
-#    }
+    sonarqube = {
+      instance_type = "t3.large"
+      policy_resource_list = []
+    }
 
     artifactory = {
       instance_type = "t3.large"
